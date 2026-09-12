9d7eddaa5d7802ba0137c2dc928008167542a3eb
thiepn <junsosmart@gmail.com>
2026-09-12 19:34:14 +0200

    Publish Atelier Space Studio 12.1.3 smooth interaction

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='atelier-space-studio-12.1.2';
+const CACHE='atelier-space-studio-12.1.3';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('atelier-space-studio-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
